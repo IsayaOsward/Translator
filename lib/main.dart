cccd43bb080d90a66bf4ad6790b2804a4e0ef6c3
Isaya Osward <ionthefirst97@gmail.com>
2024-04-27 18:49:14 +0300

    Complete app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:lang_translator/languages.dart';
 import 'package:translator/translator.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: HomePage(),
+    debugShowCheckedModeBanner: false,
   ));
 }
 
@@ -16,21 +17,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController OutputController =
+  TextEditingController outputController =
       TextEditingController(text: "Result Goes here");
   final translator = GoogleTranslator();
+  String inputText = '';
   String inputLanguage = 'en';
   String outputLanguage = 'fr';
-  String inputText = '';
-
-  Future<void> TransaltedText() async {
+  Future<void> transalteText() async {
     final translated = await translator.translate(
       inputText,
       from: inputLanguage,
       to: outputLanguage,
     );
     setState(() {
-      OutputController.text = translated.text;
+      outputController.text = translated.text;
     });
   }
 
@@ -39,7 +39,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: SafeArea(
           child: SingleChildScrollView(
-        padding: EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
@@ -48,19 +48,97 @@ class _HomePageState extends State<HomePage> {
               fit: BoxFit.contain,
               height: 250,
             ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Enter text to translate',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(2)
-                )
+            const SizedBox(
+              height: 20,
+            ),
+            const Center(
+              child: Text(
+                "Translate Your Text To Any Language",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 43, 49, 152),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
               ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+              decoration: InputDecoration(
+                  hintText: 'Enter text to translate',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(2))),
               maxLines: 5,
               onChanged: (value) {
                 setState(() {
                   inputText = value;
                 });
               },
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                DropdownButton<String>(
+                  value: inputLanguage,
+                  onChanged: (newvalue) {
+                    setState(() {
+                      inputLanguage = newvalue!;
+                    });
+                  },
+                  items: languageNames.entries
+                      .map(
+                        (item) => DropdownMenuItem<String>(
+                          value: item.key,
+                          child: Text(item.value),
+                        ),
+                      )
+                      .toList(),
+                ),
+                const Icon(Icons.arrow_forward_ios_rounded),
+                DropdownButton(
+                    value: outputLanguage,
+                    items: languageNames.entries
+                        .map(
+                          (e) => DropdownMenuItem(
+                              value: e.key, child: Text(e.value)),
+                        )
+                        .toList(),
+                    onChanged: (output) {
+                      setState(() {
+                        outputLanguage = output!;
+                      });
+                    })
+              ],
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+              onPressed: transalteText,
+              style: const ButtonStyle(
+                  minimumSize: MaterialStatePropertyAll(Size.fromHeight(55)),
+                  foregroundColor: MaterialStatePropertyAll(Colors.white),
+                  backgroundColor: MaterialStatePropertyAll(
+                      Color.fromARGB(255, 43, 49, 152))),
+              child: const Text('Translate'),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextField(
+              readOnly: true,
+              style: const TextStyle(fontSize: 20),
+              maxLines: 8,
+              controller: outputController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+              ),
             )
           ],
         ),
